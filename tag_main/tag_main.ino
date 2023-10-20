576f2fde8b2215e7dc4db6da2139c04ca06c6167
= <jack.jintao.huang@intel.com>
2023-10-19 20:05:17 -0400

    copied over setup and loop code

--- tag_main/tag_main.ino
+++ tag_main/tag_main.ino
@@ -14,12 +14,101 @@ const uint8_t PIN_SS = 21;   // spi select pin
 #define I2C_SDA 4
 #define I2C_SCL 5
 
+// leftmost two bytes below will become the "short address"
+char tag_addr[] = "7D:00:22:EA:82:60:3B:9C";
+float current_tag_position[3] = {0}; //tag current position (meters with respect to origin anchor)
+float current_distance_rmse = 0.0;  //error in distance calculations. Crude measure of coordinate error (needs to be characterized)
+
+// variables for position determination
+#define N_ANCHORS 4   //THIS VERSION WORKS ONLY WITH 4 ANCHORS. May be generalized to 5 or more.
+#define ANCHOR_DISTANCE_EXPIRED 5000   //measurements older than this are ignore (milliseconds)
+
+uint32_t last_anchor_update[N_ANCHORS] = {0}; //millis() value last time anchor was seen
+float last_anchor_distance[N_ANCHORS] = {0.0}; //most recent distance reports
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  Wire.begin(I2C_SDA, I2C_SCL);
+  delay(1000);
 
+  //initialize configuration
+  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+
+  DW1000Ranging.attachNewRange(newRange);
+  DW1000Ranging.attachNewDevice(newDevice);
+  DW1000Ranging.attachInactiveDevice(inactiveDevice);
+
+  // start as tag, do not assign random short address
+
+  DW1000Ranging.startAsTag(tag_addr, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  DW1000Ranging.loop();
+}
 
+
+void newRange()
+{
+  int i;
+
+  //index of this anchor, expecting values 1 to 4
+  int index = DW1000Ranging.getDistantDevice()->getShortAddress() & 0x07; //expect devices 1 to 7
+  if (index > 0 && index < 5) {
+    last_anchor_update[index - 1] = millis();  //(-1) => array index
+    float range = DW1000Ranging.getDistantDevice()->getRange();
+    last_anchor_distance[index-1] = range;
+    if (range < 0.0 || range > 30.0)     last_anchor_update[index - 1] = 0;  //sanity check, ignore this measurement
+  }
+
+#ifdef DEBUG_ANCHOR_ID
+  Serial.print(index); //anchor ID, raw range
+  Serial.print(" ");;
+  Serial.println(range);
+#endif
+  //check for four measurements within the last interval
+  int detected = 0;  //count anchors recently seen
+
+  for (i = 0; i < N_ANCHORS; i++) {
+
+    if (millis() - last_anchor_update[i] > ANCHOR_DISTANCE_EXPIRED) last_anchor_update[i] = 0; //not from this one
+    if (last_anchor_update[i] > 0) detected++;
+  }
+  if ( (detected == N_ANCHORS)) { //four recent measurements
+
+#ifdef DEBUG_DISTANCES
+    // print distance and age of measurement
+    uint32_t current_time = millis();
+    for (i = 0; i < N_ANCHORS; i++) {
+      Serial.print(last_anchor_distance[i]);
+      Serial.print("\t");
+      Serial.println(current_time - last_anchor_update[i]); //age in millis
+    }
+#endif
+
+    trilat3D_4A();
+    Serial.print("P= ");  //result
+    Serial.print(current_tag_position[0]);
+    Serial.write(',');
+    Serial.print(current_tag_position[1]);
+    Serial.write(',');
+    Serial.print(current_tag_position[2]);
+    Serial.write(',');
+    Serial.println(current_distance_rmse);
+  }
+}  //end newRange
+
+void newDevice(DW1000Device *device)
+{
+  Serial.print("Device added: ");
+  Serial.println(device->getShortAddress(), HEX);
+}
+
+void inactiveDevice(DW1000Device *device)
+{
+  Serial.print("delete inactive device: ");
+  Serial.println(device->getShortAddress(), HEX);
 }
