1d2fd962862a911abf86ed27824359eae69403be
Jack Huang <jintaohuang72@gmail.com>
2024-01-23 21:02:29 -0500

    added tcp server

---
+++ multitag_tcp_server.py
@@ -0,0 +1,71 @@
+import socket
+import time
+import json
+
+# Helper Functions
+def get_next_key(d, current_key):
+    keys = list(d.keys())
+    if current_key not in keys:
+        raise ValueError("The provided key is not in the dictionary.")
+    current_index = keys.index(current_key)
+    next_index = (current_index + 1) % len(keys)
+    return keys[next_index]
+
+def request_measurements(connection, message):
+    connection.send(bytes(message, 'utf-8'))
+    print(f"Sent req msg to {connection}")
+
+def current_milli_time():
+    return round(time.time() * 1000)
+
+# TCP Server Setup
+server_ip = "192.168.1.125" 
+server_port = 12345
+
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_socket.bind((server_ip, server_port))
+tcp_socket.listen(1)
+
+print(f"TCP server listening on {server_ip}:{server_port}")
+
+tag_address = {}
+start_time = 0
+previous_id = None
+
+while True:
+    connection, client_address = tcp_socket.accept()
+    try:
+        print(f"Connection from {client_address}")
+
+        while True:
+            data = connection.recv(1024)
+            if data:
+                data = json.loads(data.decode('utf-8'))
+                id = data['id']
+                ip, port = client_address
+
+                if id not in tag_address.keys():
+                    print(f"New tag added. ID: {id} Address: {ip}:{port}")
+                    tag_address[id] = client_address
+                    if len(tag_address) == 1:
+                        request_measurements(connection, "1")
+                        start_time = current_milli_time()
+                        previous_id = id
+                else:
+                    print(f"Received {len(data)} bytes from {ip}:{port}")
+                    print(f"Data: {data}")
+                    if client_address != tag_address[id]:
+                        tag_address[id] = client_address
+                    end_time = current_milli_time()
+                    time_taken = end_time - start_time
+                    print(f"Time taken (ms): {time_taken}")
+                    start_time = current_milli_time()
+
+                    next_id = get_next_key(tag_address, id)
+                    next_ip, next_port = tag_address[next_id]
+                    request_measurements(connection, "1")
+                    previous_id = next_id
+            else:
+                break
+    finally:
+        connection.close()
---
+++ multitag_udp_server.py
@@ -0,0 +1,88 @@
+"""
+Prototype server that handles multitag.
+
+Tags needs to send an initial packet to register itself with the server.
+"""
+
+import socket
+import time
+import json
+# Helper Functions
+
+# get next key of the dict, returns the first key if it reaches the end
+def get_next_key(d, current_key):
+    keys = list(d.keys())
+    if current_key not in keys:
+        raise ValueError("The provided key is not in the dictionary.")
+    current_index = keys.index(current_key)
+    next_index = (current_index + 1) % len(keys)
+    return keys[next_index]
+
+def request_measurements(socket, ip, port):
+    addr = (ip, port)
+    msg = bytes("1", 'utf-8') # or whatever message
+    socket.sendto(msg, addr)
+    print(f"Sent req msg to {ip}:{port}")
+
+def current_milli_time():
+    return round(time.time() * 1000)
+
+# Define the UDP server's IP address and port
+server_ip = "192.168.1.115" 
+server_port = 12345  # Replace with the port you want to listen on
+
+# Create a UDP socket
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the specified IP address and port
+udp_socket.bind((server_ip, server_port))
+udp_socket.settimeout(1)
+
+print(f"UDP server listening on {server_ip}:{server_port}")
+
+tag_address = {} # maps ip to port
+start_time = 0
+
+previous_id = None
+
+while True:
+    try:
+        data, (ip, port) = udp_socket.recvfrom(1024)
+    except socket.timeout:
+        print("Trying again!")
+        # if previous_id is not None:
+        #     id = get_next_key(tag_address, previous_id)
+        #     ip, port = tag_address[id]
+        #     request_measurements(udp_socket, ip, port)
+        continue
+    
+    if not data:
+        continue
+
+    data = json.loads(data.decode('utf-8'))
+    id = data['id']
+    # add to tag list if we haven't seen this tag before
+    if id not in tag_address.keys():  
+        print(f"New tag added. ID: {id} Address: {ip}:{port}")
+        tag_address[id] = (ip,port)
+        if len(tag_address) == 1:
+            request_measurements(udp_socket, ip, port)
+            start_time = current_milli_time()
+            previous_id = id
+    # have seen this before, and just got back a packet.
+    else: 
+        print(f"Received {len(data)} bytes from {ip}:{port}")
+        print(f"Data: {data}")
+        # update ip and port if its changes
+        if (ip, port) != tag_address[id]:
+            tag_address[id] = (ip, port)
+        end_time = current_milli_time()
+        time_taken = end_time - start_time
+        print(f"Time taken (ms): {time_taken}")
+        start_time = current_milli_time()
+
+        next_id = get_next_key(tag_address, id)
+        next_ip, next_port = tag_address[next_id]
+
+        request_measurements(udp_socket, next_ip, next_port)
+        previous_id = next_id
