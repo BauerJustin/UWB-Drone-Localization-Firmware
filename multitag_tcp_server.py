3787c970d45af5e8c3a9dde41e2b9ab65218897f
Jack <jintaohuang72@gmail.com>
2024-02-22 23:02:59 -0500

    tcp server currently not pass token as intended

--- multitag_tcp_server.py
+++ multitag_tcp_server.py
@@ -2,6 +2,7 @@ import socket
 import time
 import json
 import threading
+from collections import defaultdict
 
 # Helper Functions
 def get_next_key(d, current_key):
@@ -19,43 +20,40 @@ def request_measurements(connection, message):
 def current_milli_time():
     return round(time.time() * 1000)
 
-def handle_connection(connection, client_address, tag_address, lock):
-    try:
-        print(f"Connection from {client_address}")
-
-        while True:
-            data = connection.recv(1024)
-            if data:
-                data = json.loads(data.decode('utf-8'))
-                id = data['id']
-                ip, port = client_address
-
-                with lock:
-                    if id not in tag_address.keys():
-                        print(f"New tag added. ID: {id} Address: {ip}:{port}")
-                        tag_address[id] = client_address
-                        if len(tag_address) == 1:
-                            request_measurements(connection, "1")
-                            start_time = current_milli_time()
-                            previous_id = id
-                    else:
-                        print(f"Received {len(data)} bytes from {ip}:{port}")
-                        print(f"Data: {data}")
-                        if client_address != tag_address[id]:
-                            tag_address[id] = client_address
-                        end_time = current_milli_time()
-                        time_taken = end_time - start_time
-                        print(f"Time taken (ms): {time_taken}")
-                        start_time = current_milli_time()
-
-                        next_id = get_next_key(tag_address, id)
-                        next_ip, next_port = tag_address[next_id]
-                        request_measurements(connection, "1")
-                        previous_id = next_id
-            else:
-                break
-    finally:
+def handle_connection(connection, client_address, last_update_timestamp, lock):
+    connection.settimeout(5)
+    last_timestamp = None
+    print(f"Connection from {client_address}")
+    data = connection.recv(1024)
+    if not data:
+        print("No data received.")
         connection.close()
+        return
+    id = json.loads(data.decode('utf-8'))['id']
+
+    while True:
+        with lock:
+            try:
+                connection.sendall(bytes("1", 'utf-8')) # request data
+                data = connection.recv(1024)
+            except:
+                print(f"Timeout occurred by {id}.")
+                connection.close()
+                return
+            if not data:
+                break
+            
+            data = json.loads(data.decode('utf-8'))
+            id = data['id']
+            current_timestamp = time.time()
+            last_timestamp = last_update_timestamp[id]
+            time_difference = (current_timestamp - last_timestamp)
+            frequency = 1 / time_difference
+
+            print(f"Update frequency for ID {id}: {frequency} Hz")
+            print(f"Data: {data}")
+
+            last_update_timestamp[id] = current_timestamp
 
 # TCP Server Setup
 server_ip = "192.168.0.219" 
@@ -67,13 +65,13 @@ tcp_socket.listen(5)  # Listen for multiple connections
 
 print(f"TCP server listening on {server_ip}:{server_port}")
 
-tag_address = {}
+last_update_timestamp = defaultdict(int)
 lock = threading.Lock()
 
 try:
     while True:
         connection, client_address = tcp_socket.accept()
-        client_thread = threading.Thread(target=handle_connection, args=(connection, client_address, tag_address, lock))
+        client_thread = threading.Thread(target=handle_connection, args=(connection, client_address, last_update_timestamp, lock))
         client_thread.start()
 except KeyboardInterrupt:
     print("Server is shutting down.")
